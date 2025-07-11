082479012613ea2340da33fc8d5612899043b847
adam <awhiles2011@gmail.com>
2025-07-11 15:08:59 -0500

    fix eza install - removed from dnf in fedora 42

--- install/terminal/apps-terminal.sh
+++ install/terminal/apps-terminal.sh
@@ -1,1 +1,7 @@
-sudo dnf install -y fzf ripgrep bat eza zoxide plocate btop httpd-tools fd-find tldr
+sudo dnf install -y fzf ripgrep bat zoxide plocate btop httpd-tools fd-find tldr
+
+wget -O eza.tar.gz https://github.com/eza-community/eza/releases/latest/download/eza_aarch64-unknown-linux-gnu.tar.gz
+tar -xf eza.tar.gz
+sudo install eza /usr/local/bin
+rm eza eza.tar.gz
+cd -
