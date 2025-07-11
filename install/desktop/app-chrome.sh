849d9c7bcb910575c4bab55b71ef50bbcbe81879
adam <awhiles2011@gmail.com>
2025-07-11 15:45:39 -0500

    fix error in default browser setup

--- install/desktop/app-chrome.sh
+++ install/desktop/app-chrome.sh
@@ -1,4 +1,4 @@
 # Browse the web with the most popular browser. See https://www.google.com/chrome/
 sudo dnf install -y chromium
-xdg-settings set default-web-browser chromium.desktop
+xdg-settings set default-web-browser chromium-browser.desktop
 cd -
