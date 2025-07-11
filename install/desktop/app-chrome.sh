80c1ef618ece3fc32f77a2d3bbaf214b6ef61d42
adam <awhiles2011@gmail.com>
2025-07-11 15:37:39 -0500

    trade out chrome for chromium

--- install/desktop/app-chrome.sh
+++ install/desktop/app-chrome.sh
@@ -1,4 +1,4 @@
 # Browse the web with the most popular browser. See https://www.google.com/chrome/
-sudo dnf install -y google-chrome-stable
-xdg-settings set default-web-browser google-chrome.desktop
+sudo dnf install -y chromium
+xdg-settings set default-web-browser chromium.desktop
 cd -
